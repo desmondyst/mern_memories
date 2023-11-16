a0cc6713eae51bc0f0e706c0a605bb9c17ae2eab
desmondyst <desmondyong06@gmail.com>
2023-11-16 10:42:04 +0800

    :ambulance: swap to less overloaded cluster

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,9 +4,9 @@ import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
     try {
-        const postMessage = await PostMessage.find();
+        const postMessages = await PostMessage.find();
 
-        res.status(200).json(postMessage);
+        res.status(200).json(postMessages);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
